26d42b88a927468b7084a733ce420cd903e5253b
Straitspartners <projectcare@straitspartners.co>
2025-03-01 18:23:29 +0530

    srttings

--- Backend/only2bali/settings.py
+++ Backend/only2bali/settings.py
@@ -35,7 +35,7 @@ CSRF_TRUSTED_ORIGINS=['https://'+os.environ['pybackend-eeamcqf4evb6hacn.centrali
 
 # AVIATIONSTACK_API_KEY = '77a30f0f78f6d8f3e75b6089a9427308'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # Email configuration (adjust for your email service provider)
 # #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Use SMTP for real emails
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
