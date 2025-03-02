1542865a342997a6bf9fd71c0cb5fb26154d0112
Straitspartners <projectcare@straitspartners.co>
2025-03-02 16:14:08 +0530

    routes.json

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';  // Make sure ReactDOM is imported
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom'; 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   document.getElementById('root')
 );
